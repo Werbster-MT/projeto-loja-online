59d6733e646beaccb72f62a3cca82924e5d37d16
Werbster <wmteixeira.88@gmail.com>
2025-01-27 21:59:23 -0300

    updating builspec.

--- deploy-scripts/application-start-hook.sh
+++ deploy-scripts/application-start-hook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Verificando conteúdo de /var/www/codeigniter-app..."
-ls -la /var/www/codeigniter-app
+ls -la /var/www/codeigniter-app || echo "Diretório não encontrado!"
 
 sudo service nginx restart
