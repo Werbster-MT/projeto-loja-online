bdc1b3fefdb547ce831e2ed3e4f8ea113652dbc2
Werbster Marques Teixeira <69735898+Werbster-MT@users.noreply.github.com>
2025-01-28 20:25:42 -0300

    Update and rename carrinhoController.php to CarrinhoController.php

--- application/controllers/CarrinhoController.php
+++ application/controllers/CarrinhoController.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class carrinhoController extends CI_Controller{
+class CarrinhoController extends CI_Controller{
     public function index()
 	{   
         $this->load->model("usuarios_model");
