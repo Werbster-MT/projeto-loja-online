da6cd6b11ad22ab157d3f4257068b054a4aac38e
JoseMatheus29 <mateus.alvino.101@gmail.com>
2025-02-03 15:52:23 -0300

    Testing base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://ec2-18-200-234-216.eu-west-1.compute.amazonaws.com/';
+$config['base_url'] = 'http://ec2-18-200-234-216.eu-west-1.compute.amazonaws.com/';
  
 /*
 |--------------------------------------------------------------------------
